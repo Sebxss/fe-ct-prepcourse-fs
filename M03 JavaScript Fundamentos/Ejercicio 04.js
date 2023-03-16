5706399e7bbab7ee48d3a42b494bf3d42d69b4b0
sebxxs <castrosebas804@gmail.com>
2023-03-15 21:21:59 -0500

    Primer Homework terminada

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,43 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   var cuadrado = Math.pow (num, 2);
+   return cuadrado;
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   var cubo = Math.pow(num, 3);
+  return cubo;
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   let expo = Math.pow(num, exponent);
+   return expo; 
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   let resul = Math.round(num); //Math.round sirve para redondear numeros en JavaScript
+   return resul;
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   let redondo = Math.ceil(num); //----> El Math.ceil sirve para redondear el numerohacia arriba
+   return redondo;
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var resul2 = Math.random();
+ return resul2;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
